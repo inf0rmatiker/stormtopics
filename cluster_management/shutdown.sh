a7d11bf765948348280591f056d6391cd4a10db2
Caleb Carlson <ccarlson355@gmail.com>
2022-02-27 14:02:48 -0700

    Add shutdown script

--- cluster_management/shutdown.sh
+++ cluster_management/shutdown.sh
@@ -12,3 +12,15 @@ function print_usage {
 [ ! -f "./nimbus" ]    && echo "No ./nimbus file found! Exiting" && exit 1
 [ ! -f "./zookeeper" ] && echo "No ./zookeeper file found! Exiting" && exit 1
 
+echo -e "Shutting down Storm Workers..."
+while read -r WORKER; do
+  echo "Shutting down worker on $WORKER"
+  ssh -n "$WORKER" "WORKER_PID=\$(ps -aux | grep \"[o]rg.apache.storm.daemon.supervisor.Supervisor\") && echo \"Found worker process \$WORKER_PID\" && kill \$WORKER_PID || echo \"Didn't find any workers!\""
+  echo "Shutting down supervisord process on $WORKER"
+  ssh -n "$WORKER" "WORKER_PID=\$(ps -aux | grep \"/bin/supervisord\") && echo \"Found supervisord process \$WORKER_PID\" && kill \$WORKER_PID || echo \"Didn't find any supervisord processes!\""
+done < "./workers"
+
+echo -e "Shutting down Storm Nimbus on localhost..."
+while read -r WORKER; do
+  NIMBUS_PID=$(ps -aux | grep \"[o]rg.apache.storm.daemon.supervisor.Supervisor\")
+done < "./workers"
