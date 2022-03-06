034e46a7328c39aa5d69ce50b190db67eeef8191
Caleb <ccarlson355@gmail.com>
2022-03-06 15:16:52 -0700

    Durrr

--- submit.sh
+++ submit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn clean install package && storm jar target/stormtopics-1.0-SNAPSHOT.jar WordCountTopology
+mvn clean install package && storm jar target/stormtopics-1.0-SNAPSHOT.jar WordCountTopology remote
