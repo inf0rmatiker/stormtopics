0d854f34f60d25a42fee159c79efb6ce6bd60f9e
Caleb Carlson <ccarlson355@gmail.com>
2022-03-07 14:55:07 -0700

    Add TwitterSpout

---
+++ src/main/java/twitter/TwitterSpout.java
@@ -0,0 +1,80 @@
+package twitter;
+
+import org.apache.storm.spout.SpoutOutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichSpout;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.tuple.Values;
+import org.apache.storm.utils.Utils;
+
+import twitter4j.*;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
+
+import java.util.Map;
+
+public class TwitterSpout extends BaseRichSpout {
+
+    private static final String API_KEY = System.getenv("TWITTER_API_KEY");
+    private static final String API_KEY_SECRET = System.getenv("TWITTER_API_KEY_SECRET");
+    private static final String BEARER_TOKEN = System.getenv("TWITTER_BEARER_TOKEN");
+    private static final String ACCESS_TOKEN = System.getenv("TWITTER_ACCESS_TOKEN");
+    private static final String ACCESS_TOKEN_SECRET = System.getenv("TWITTER_ACCESS_TOKEN_SECRET");
+
+
+    private TwitterStream twitterStream;
+    private SpoutOutputCollector collector;
+    private StatusListener statusListener;
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("sentence"));
+    }
+
+    @Override
+    public void open(Map<String, Object> config, TopologyContext context, SpoutOutputCollector collector) {
+        this.collector = collector;
+
+        this.statusListener = new StatusListener() {
+
+            @Override
+            public void onStatus(Status status) {
+                System.out.println(status.getUser().getName() + " : " + status.getText());
+            }
+
+            @Override
+            public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {}
+
+            @Override
+            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {}
+
+            @Override
+            public void onScrubGeo(long l, long l1) {}
+
+            @Override
+            public void onStallWarning(StallWarning stallWarning) {}
+
+            @Override
+            public void onException(Exception ex) {
+                ex.printStackTrace();
+            }
+        };
+
+        ConfigurationBuilder cb = new ConfigurationBuilder()
+                .setJSONStoreEnabled(true)
+                .setDebugEnabled(true)
+                .setOAuthConsumerKey(API_KEY)
+                .setOAuthConsumerSecret(API_KEY_SECRET)
+                .setOAuthAccessToken(ACCESS_TOKEN)
+                .setOAuthAccessTokenSecret(ACCESS_TOKEN_SECRET);
+
+        this.twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
+    }
+
+    @Override
+    public void nextTuple() {
+
+    }
+
+}
--- src/main/java/twitter/TwitterTopology.java
+++ src/main/java/twitter/TwitterTopology.java
@@ -9,8 +9,10 @@ import org.apache.storm.StormSubmitter;
 import org.apache.storm.generated.StormTopology;
 import org.apache.storm.topology.TopologyBuilder;
 import org.apache.storm.tuple.Fields;
+import twitter4j.conf.ConfigurationBuilder;
 
 public class TwitterTopology {
+
 
     private static final String SENTENCE_SPOUT_ID = "sentence-spout";
     private static final String SPLIT_BOLT_ID = "split-bolt";
@@ -28,6 +30,7 @@ public class TwitterTopology {
 
     public static void main(String[] args) {
         printArgs(args);
+
         System.out.println("Running main()");
         boolean is_remote = false;
         if (args.length == 1) {
@@ -36,45 +39,7 @@ public class TwitterTopology {
             }
         }
 
-        RandomSentenceSpout spout = new RandomSentenceSpout();
-        SplitSentence splitBolt = new SplitSentence();
-        WordCount countBolt = new WordCount();
-        ReportBolt reportBolt = new ReportBolt();
-        TopologyBuilder builder = new TopologyBuilder();
 
-        builder.setSpout(SENTENCE_SPOUT_ID, spout);
-
-        // SentenceSpout --> SplitSentenceBolt
-        builder.setBolt(SPLIT_BOLT_ID, splitBolt).shuffleGrouping(SENTENCE_SPOUT_ID);
-        builder.setBolt(COUNT_BOLT_ID, countBolt).fieldsGrouping(SPLIT_BOLT_ID, new Fields("word"));
-
-        // WordCountBolt --> example.ReportBolt
-        builder.setBolt(REPORT_BOLT_ID, reportBolt).globalGrouping(COUNT_BOLT_ID);
-        StormTopology topology = builder.createTopology();
-
-        Config config = new Config();
-        System.out.println("Got here");
-        try {
-
-            if (is_remote) {
-                System.out.println("is_remote=True");
-                config.setDebug(true);
-                config.setMaxTaskParallelism(1);
-                config.setNumWorkers(4);
-                config.setMessageTimeoutSecs(60);
-                StormSubmitter.submitTopology(TOPOLOGY_NAME, config, topology);
-            }
-//            else {
-//                LocalCluster cluster = new LocalCluster();
-//                cluster.submitTopology(TOPOLOGY_NAME, config, topology);
-//                Thread.sleep(10000);
-//                cluster.killTopology(TOPOLOGY_NAME);
-//                cluster.shutdown();
-//            }
-
-        } catch (Exception e) {
-            System.err.println("Caught Exception! " + e.getMessage());
-        }
     }
 
 }
