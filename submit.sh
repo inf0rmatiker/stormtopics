0b2bc60cdb5afa0c5b43aa15defaddfce4ae0aec
Caleb <ccarlson355@gmail.com>
2022-03-07 20:04:13 -0700

    Update submit script

--- submit.sh
+++ submit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn clean install package && storm jar target/stormtopics-1.0-SNAPSHOT-jar-with-dependencies.jar WordCountTopology remote
+mvn clean install package && storm jar target/stormtopics-1.0-SNAPSHOT-jar-with-dependencies.jar twitter.TwitterTopology remote
