b0e9a1173ae6abc51fd6a552db7a6bbfed73a69a
Caleb <ccarlson355@gmail.com>
2022-03-21 10:57:50 -0600

    Change to .zshrc

--- cluster_management/start_all.sh
+++ cluster_management/start_all.sh
@@ -29,7 +29,7 @@ sleep 5
 
 while read -r WORKER; do
   echo "Starting worker on $WORKER"
-  ssh -n "$WORKER" "source ~/.bashrc && supervisord -c $STORM_HOME/worker-supervisord.conf"
+  ssh -n "$WORKER" "source ~/.zshrc && supervisord -c $STORM_HOME/worker-supervisord.conf"
   sleep 5
 done < "./workers"
 
